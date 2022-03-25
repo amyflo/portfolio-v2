445ac061feef219c632966b612b04898f9aad778
amyflo <amyflo@stanford.edu>
2022-03-25 00:38:49 -0700

    adding hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./hero.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Container } from "react-bootstrap";
+import { Link } from "gatsby"
+import { Button, Container } from "react-bootstrap";
+import { StaticImage } from "gatsby-plugin-image";
 
 function Hero(props) {
   return (
     <Container className="hero-background">
       <Row >
+
+        <Col md={3}>
+          <StaticImage src="./avatar/normal.png" />
+        </Col>
         <Col md={9}>
           <h1>
-            Hello, I'm Amy Lo. <br/> 
-            UX Designer and UX Engineer. 
+            Hello, I'm Amy Lo. <br />
+            UX Designer and UX Engineer.
           </h1>
           <p className="hero-p">{props.intro}</p>
-        </Col>
-        <Col md={3}>
-          {/* <img src="https://i.imgur.com/kDDFvUp.png" class="rotate" width="200" height="200" /> */}
+          <p className="hero-p">{props.job}</p>
         </Col>
       </Row>
     </Container>
